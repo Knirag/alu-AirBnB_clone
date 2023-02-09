92c5d2fb4e44691f1bf20edfe9fcda578b1e6079
Knirag <105249761+Knirag@users.noreply.github.com>
2023-02-09 23:25:08 +0200

    Tasks ig

---
+++ models/engine/file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Storage File(Serialization & Deserialization)"""
+
+import json
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
+from models.place import Place
