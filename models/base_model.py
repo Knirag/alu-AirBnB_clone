77d7ee81b777a2bfbbef496774b1822ab8dbb7fc
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2023-02-10 00:22:04 +0300

    class basemodel fix

--- models/base_model.py
+++ models/base_model.py
@@ -10,54 +10,32 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         '''' Initializes attributes:date,uuid4'''
         date_format = '%Y-%m-%dT%H:%M:%S.%f'
-        if kwargs:
+        if kwargs.__len__() > 0:
             for key, value in kwargs.items():
-                if "created_at" == key:
-                    self.created_at = datetime.strptime(kwargs["created_at"],
-                                                        date_format)
-                elif "updated_at" == key:
-                    self.updated_at = datetime.strptime(kwargs["updated_at"],
-                                                        date_format)
-                elif "__class__" == key:
-                    pass
-                else:
+                if key == 'created_at' or key == 'updated-at':
+                    value  = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+                    continue
+                if key != '__class__':
                     setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
-        """
-        Return id, class name and dictionary
-        """
-        return ('[{}] ({}) {}'.
-                format(self.__class__.__name__, self.id, self.__dict__))
-
-    def __repr__(self):
-        """
-        returns string repr
-        """
-        return (self.__str__())
+        class_name = "[" + self.__class__.name__ +"]"
+        1st = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
+        return class_name + " (" + self.id + ") " + str(1st)
 
     def save(self):
-        """
-        Instance method to:
-        - update current datetime
-        - invoke save() function &
-        - save to serialized file
-        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """
-        Return dictionary of BaseModel with string formats of times
-        """
-        dic = self.__dict__.copy()
-        dic["created_at"] = self.created_at.isoformat()
-        dic["updated_at"] = self.updated_at.isoformat()
-        dic["__class__"] = self.__class__.__name__
-        return dic
-
+        date = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
