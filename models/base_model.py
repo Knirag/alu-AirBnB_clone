de0e56bd16e93d0c08cd44e9587a7a92f9e4cd06
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2023-03-08 14:37:40 +0300

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,19 @@
-#!usr/bin/python3
-'''class basemodel that defines all common attributes/methods'''
+#!/usr/bin/python3
+"""Our BaseModel that defines all common attributes/methods for other classes
+"""
 
-from uuid import uuid4
+import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
-    '''parent class for all classes'''
-    def __init__(self, *args, **kwargs):
-        '''' Initializes attributes:date,uuid4'''
-        date_format = '%Y-%m-%dT%H:%M:%S.%f'
+    """contains common attribute"""
+    def __init__(self, *arg, **kwargs):
         if kwargs.__len__() > 0:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated-at':
-                    value  = datetime.fromisoformat(value)
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
                     setattr(self, key, value)
                     continue
                 if key != '__class__':
@@ -25,16 +25,16 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        class_name = "[" + self.__class__.name__ +"]"
-        1st = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
-        return class_name + " (" + self.id + ") " + str(1st)
+        class_name = "[" + self.__class__.__name__ + "]"
+        lst = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
+        return class_name + " (" + self.id + ") " + str(lst)
 
     def save(self):
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        date = self.__dict__.copy()
+        data = self.__dict__.copy()
         data['__class__'] = self.__class__.__name__
         data['created_at'] = self.created_at.isoformat()
         data['updated_at'] = self.updated_at.isoformat()
